fba04a6090476c514d29dc7e4fe5d6c9ca3002c9
Henry <henryhobhouse@gmail.com>
2017-03-25 12:59:19 +0000

    First attempt at exercise 5

--- ex04/01.rb
+++ ex04/01.rb
@@ -2,7 +2,7 @@
 cars = 100
 
 # capacity of each car
-space_in_a_car = 4.0
+space_in_a_car = 4
 
 # number of drivers
 drivers = 30
@@ -29,3 +29,5 @@ puts "There will be #{cars_not_driven} empty cars today."
 puts "We can transport #{carpool_capacity} people today."
 puts "We have #{passengers} to carpool today."
 puts "We need to put about #{average_passengers_per_car} in each car."
+
+# error on exercise in on line 14 'puts "We can transport #{carpool_capacity} people today."' it doesn't recognise the variable suggesting it wasn't set correctly.
