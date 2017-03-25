b4926400aadcadc2b7f1b296bf409dfb8e62d7d1
Henry <henryhobhouse@gmail.com>
2017-03-25 14:56:21 +0000

    added exercise 9

---
+++ ex09/01.rb
@@ -0,0 +1,17 @@
+# adds days variable and lists them as a string
+days = "Mon Tues Wed Thu Fris Sat Sun"
+#adds a months variable, lists as a string although has a 'n' in front of the month abrevaitions
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# displays days on one line
+puts "Here are the days: #{days}"
+# displays months on one line, i think
+puts "Here are the months: #{months}"
+
+# not sure what this does
+puts %q{
+  There's something going on here.
+  With the three double-quotes.
+  We'll be able to type as much as we like.
+  Even 4 lines if we want, or 5, or 6.
+}
