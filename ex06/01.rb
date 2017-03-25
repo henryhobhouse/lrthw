2a14273fd329a15d8d5c8a27d4caa538fed04986
Henry <henryhobhouse@gmail.com>
2017-03-25 14:05:31 +0000

    Adding finished exercise 6

--- ex06/01.rb
+++ ex06/01.rb
@@ -1,21 +1,32 @@
-type_of_people = 10
+# sets types_of_people variable as a integer
+types_of_people = 10
+# shortens types_of_people variable and makes it a string
 x = "There are #{types_of_people} types of people."
+# setting two variables, as strings, with schemantic changes to naming
 binary = "binary"
 do_not = "don't"
+# shortens user binary and do_not vairables into new string
 y = "Those who know #{binary} and those who #{do_not}."
 
+#puts x and y variable to the user
 puts x
 puts y
 
+# adding x and y variables into new strings
 puts "I said: #{x}"
 puts "I also said: '#{y}'."
 
-hilarious = false
+# setting new hilarious variable as a logic statement (false)
+hilarious = true
+# adding hilarious into a new string
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# puts newly created joke_evaluation string
 puts joke_evaluation
 
+# creating two new strings with short names
 w = "This is the left side of..."
 e = "a string with a right side."
 
+# putting two strings together.
 puts w + e
