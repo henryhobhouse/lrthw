f43a6cbb6c6f77b9e85a99482bebb5c913d42df9
Henry <henryhobhouse@gmail.com>
2017-03-25 14:23:15 +0000

    finsihed exercise 7

--- ex07/01.rb
+++ ex07/01.rb
@@ -1,8 +1,11 @@
+# puts three lines of rhyme 'mary had a little line'
 puts "Mary had a little lamb."
-puts "Its fleexe was white as #{'snow'}"
+puts "Its fleece was white as #{'snow'}"
 puts "And everywhere that Mary went."
+# puts a '.' 10 times in a row
 puts "." * 10 # what'd that do?
 
+# creates 12 new variables spelling cheeseburger
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -16,7 +19,7 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# watch that print vs. puts on this line what's it do?
+# display cheeseburger, as print is only the first half it means the word is over a single line
 print end1 + end2 + end3 + end4 + end5 + end6
 
 puts end7 + end8 + end9 + end10 + end11 + end12
